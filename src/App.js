9ec9cfe6992cf85ba79eee8689ca2ba92cfa36b2
Sanja <codesanja@gmail.com>
2018-10-02 15:24:03 +0100

    Adding router.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import logo from './logo.png';
 import './App.css';
 import Home from './components/Home';
+import Vitamin from './components/Vitamin';
 import Navigation from './components/Navigation';
 
 class App extends Component {
@@ -9,7 +12,7 @@ class App extends Component {
     super(props);
     this.state = {
       toggleLogo: true,
-    }
+    };
     this.toggleLogo = this.toggleLogo.bind(this);
     this.openNav = this.openNav.bind(this);
     this.closeNav = this.closeNav.bind(this);
@@ -31,23 +34,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} 
-            className={this.state.toggleLogo ? 'static-logo' : 'static-logo animated jello'} 
-            alt="logo"
-            onMouseEnter={this.toggleLogo}
-            onMouseLeave={this.toggleLogo}
-            onClick={this.openNav}
-          />
-          <h1
-            className={this.state.toggleLogo ? 'menu-hidden' : 'menu animated bounceInDown'}
-            onClick={this.openNav}
-          >Menu</h1>
-          <Navigation closeNav={this.closeNav} />
-        </header>
-        <Home />
-      </div>
+      <Router>
+          <div className="App">
+            <header className="App-header">
+                <img src={logo}
+                     className={this.state.toggleLogo ? 'static-logo' : 'static-logo animated jello'}
+                     alt="logo"
+                     onMouseEnter={this.toggleLogo}
+                     onMouseLeave={this.toggleLogo}
+                     onClick={this.openNav}
+                />
+                <h1
+                    className={this.state.toggleLogo ? 'menu-hidden' : 'menu animated bounceInDown'}
+                    onClick={this.openNav}
+                >Menu</h1>
+                <Navigation closeNav={this.closeNav} />
+            </header>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/vitamin" component={Vitamin} />
+            </Switch>
+          </div>
+      </Router>
     );
   }
 }
